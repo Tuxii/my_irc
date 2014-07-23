13cbe86738fe489225b126f09f801675c7773b95
= <=>
2014-07-23 08:01:07 +0200

    add login

--- public/client.js
+++ public/client.js
@@ -1,17 +1,25 @@
 var socket = io()
 
-$('.login-form').submit(function(e){
+$('#login-form').submit(function(e){
 	e.preventDefault()
 	$email = $('#email').val().trim()
 	$nickname = $('#nickname').val().trim()
-	socket.emit('login', {email: $email, nickname: $nickname}, function(valid){
-		if(valid){
+	socket.emit('login', {email: $email, nickname: $nickname}, function(msg){
+		if(!msg){
 			// hide login form
+			$('.login').fadeOut()
 		}
 		else{
-			//display error
+			$('#login-message').html(msg).fadeIn()
 		}
 	})
 
+
+})
+
+socket.on('updateUsersList', function(lol){
+	$('#users-number').html(lol);
+	$('#users-number').html(Mustache.render($('#users-number').html(), lol));
+	alert(Mustache.render($('#users-number').html(), lol))
 
 })
--- server.js
+++ server.js
@@ -17,21 +17,55 @@ app.get('/', function(request, response){
 });
 
 var client
-var user = {}
-var users = []
+var sockets = {}
+var users = {}
+var nbUsers = 0
+var nicknames = []
 
 io.on('connection', function(socket){
 
-	socket.on('login', function(user){
-		socket.nickname = user.nickname
-		socket.avatar = 'https://gravatar.com/avatar/' + md5(user.email) + '?s=50'
-		users.push[nickname];
-		user
+
+	socket.on('login', function(user, callback){
+		// user already exists
+		if(user.nickname in sockets){
+			callback("Votre pseudo est déjà pris !")
+		}
+		else
+		{
+			if(user.nickname.length < 3){
+				callback("Votre pseudo est trop court !")
+			}
+			else if(!validateEmail(user.email)){
+				callback("Veuillez entrer une adresse e-mail valide !")
+			}
+			else{
+				// user is valid and can now log in
+				callback(false)
+				user.nickname = user.nickname
+				user.avatar = 'https://gravatar.com/avatar/' + md5(user.email) + '?s=50'
+				sockets[user.nickname] = socket
+				users[user.nickname] = user
+				++nbUsers
+				console.log(users)
+				nicknames.push(users[i].nickname)
+				console.log(nicknames)
+				io.emit('updateUsersList',nbUsers, nicknames);
+			}
+		}
 	})
+
+
+
+	socket.on('disconnect', function(){
+		if(!socket.nickname) return;
+		delete sockets[socket.nickname]
+		--nbUsers
+	})
+
 })
 
 
-
-function checkNickname(nickname){
-	return users.indexOf(nickname) != -1
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
