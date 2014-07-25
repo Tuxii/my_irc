43d69775ab40a2d7d567cf7320e421a881d34988
= <=>
2014-07-25 10:43:29 +0200

    trokoul

--- public/client.js
+++ public/client.js
@@ -1,5 +1,7 @@
 var socket = io()
 
+
+// connection
 $('#login-form').submit(function(e){
 	e.preventDefault()
 	$email = $('#email').val().trim()
@@ -18,15 +20,64 @@ $('#login-form').submit(function(e){
 
 })
 
-socket.on('updateUsersList', function(lol, data){
 
-	var test= {}
-	test.nickname = "test"
-	console.log(data)
-	console.log(JSON.stringify(data));
-	console.log(Mustache.render($('#users-list').html(), test))
- 	$('#users-list').html(Mustache.render($('#users-list').html(), {users: JSON.stringify(data)}));
+socket.on('newUser', function(data){
+	$('#messages-list').append(data+ ' vient de se connecter');
+})
 
+// update the number of users and print their names
+socket.on('updateUsersList', function(nbUsers, user){
+	// var $usertpl = $('#user-tpl').html();
+	// $('#user-tpl').remove()
+	// // $('#users-list').append(Mustache.render($usertpl, user));
+	// update number of users
+	$('#users-number').html(nbUsers)
 
+	// add user to the list
+	$('#users-list').append('<li id="'+user.id+'" class="users-item">\
+        <div class="users-avatar">\
+            <img src="'+user.avatar+'" alt="">\
+        </div>\
+        <div class="users-name">\
+            <p>'+user.nickname+'</p>\
+        </div>\
+    </li>')
+
+ 	// $('#users-list').html(
+ 	// 	Mustache.render($('#users-list').html(), {users: users})
+ 	// );
 
 })
+
+socket.on('delUser', function(user){
+	$('#'+user.id).remove();
+})
+
+//send message
+$('#messages-form').submit(function(e){
+	e.preventDefault();
+	$message = $('#message').val();
+	if($message.length > 0){
+		socket.emit('sendMessage', {message : $message})
+	}
+	$('#message').val('');
+
+})
+
+// receive message
+socket.on('newMessage', function(message){
+	$('#messages-list').append('<li class="messages-item">\
+        <div class="messages-message">\
+            <div class="messages-avatar">\
+                <img src="'+message.avatar+'">\
+            </div>\
+            <div class="messages-content">\
+                <p><span class="messages-author">'+message.nickname+'</span><span class="messages-date">'+message.h+':'+message.m+'</span></p>\
+                <p>'+message.msg+'</p>\
+            </div>\
+        </div>\
+    </li>')
+    $("#messages-list").animate({ scrollTop: $('#messages-list').prop('scrollHeight') }, 0)
+})
+
+
--- server.js
+++ server.js
@@ -2,31 +2,33 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var md5 = require('md5');
+
 var gravatar = require('gravatar')
-var port = process.env.PORT || 1337;
+var port =  1337;
+
 
 http.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
-app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public');
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response){
-	response.render('index.ejs');
+	response.render('index.html');
 });
 
-var client
 var sockets = {}
 var users = {}
 var nbUsers = 0
 
 io.on('connection', function(socket){
 
-
+	// user already exists
+	for(var i in users){
+		socket.emit('updateUsersList', nbUsers, users[i])
+	}
+	var me = {}
 	socket.on('login', function(user, callback){
-		// user already exists
 		if(user.nickname in sockets){
 			callback("Votre pseudo est déjà pris !")
 		}
@@ -35,30 +37,66 @@ io.on('connection', function(socket){
 			if(user.nickname.length < 3){
 				callback("Votre pseudo est trop court !")
 			}
-			else if(!validateEmail(user.email)){
-				callback("Veuillez entrer une adresse e-mail valide !")
-			}
+			// else if(!validateEmail(user.email)){
+			// 	callback("Veuillez entrer une adresse e-mail valide !")
+			// }
 			else{
 				// user is valid and can now log in
 				callback(false)
-				user.nickname = user.nickname
 				user.avatar = gravatar.url(user.email, {s:60})
-				// user.avatar = 'https://gravatar.com/avatar/' + md5(user.email) + '?s=50'
+				user.id = socket.id
+				me = user
+				socket.nickname = user.nickname
 				sockets[user.nickname] = socket
-				users[user.nickname] = user
+				users[user.email] = user
+				console.log(user)
+				console.log(me)
 				++nbUsers
-				console.log(users)
-				io.emit('updateUsersList', nbUsers, users);
+				io.emit('updateUsersList', nbUsers, me);
+				// for(var i in sockets){
+				// 	if(i != socket.nickname){
+				// 		sockets[i].emit('newUser', socket.nickname);
+				// 	}
+				// 	// sockets[i].emit('updateUsersList', nbUsers, users);
+				// }
 			}
 		}
 	})
 
+	// for(var i in users){
+	// 	socket.emit('updateUsersList', nbUsers, users[i])
+	// }
 
+	socket.on('sendMessage', function(message){
+		date = new Date();
+		io.emit('newMessage', {
+			msg : message.message,
+			nickname : socket.nickname,
+			avatar : socket.avatar,
+			h: date.getHours(),
+			m: date.getMinutes()
+		})
+	})
 
-	socket.on('disconnect', function(){
+	// refresh or exit browser
+	socket.on('disconnect', function(data){
 		if(!socket.nickname) return;
+
+
+		delete users[me.email]
 		delete sockets[socket.nickname]
+		io.emit('delUser', me)
 		--nbUsers
+		// if(typeof users != "undefined" && users.length > 0){
+		// 	for(var i=0; i< users.length; i++){
+		// 		if(users[i].nickname == socket.nickname){
+		// 			// empty users array when disconnect
+		// 			users.splice(i, 1)
+		// 		}
+		// 	}
+		// }
+
+		// socket.broadcast.emit('updateUsersList', nbUsers, users);
 	})
 
 })
