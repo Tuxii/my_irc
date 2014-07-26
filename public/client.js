32c7c9b6776abc2bf31ad41ef05fd056db65bd6f
= <=>
2014-07-26 13:35:45 +0200

    presque fini

--- public/client.js
+++ public/client.js
@@ -1,83 +1,223 @@
-var socket = io()
+$( document ).ready(function(){
+
+	var socket = io()
+	var username
+
+	// connection
+	$('#login-form').submit(function(e){
+		e.preventDefault()
+		$email = $('#email').val().trim()
+		$nickname = $('#nickname').val().trim()
+		socket.emit('login', {email: $email, nickname: $nickname}, function(msg){
+			if(!msg){
+				// hide login form
+				$('.login').fadeOut()
+				$('.overlay').fadeOut()
+				$('#username').html($nickname)
+				username = $nickname
+			}
+			else{
+				$('#login-message').html(msg).fadeIn()
+			}
+		})
 
 
-// connection
-$('#login-form').submit(function(e){
-	e.preventDefault()
-	$email = $('#email').val().trim()
-	$nickname = $('#nickname').val().trim()
-	socket.emit('login', {email: $email, nickname: $nickname}, function(msg){
-		if(!msg){
-			// hide login form
-			$('.login').fadeOut()
-			$('.overlay').fadeOut()
-		}
-		else{
-			$('#login-message').html(msg).fadeIn()
-		}
 	})
 
 
-})
+	socket.on('newUser', function(user){
+		// add notification to all other users about new user logged
+		if(user.present){
+			$('#messages-list').append('<li class="messages-item new-user"><strong>'+ user.nickname+ '</strong> vient de se connecter<span class="messages-date">'+user.h+':'+user.m+'</span></li>');
+		}
+		else{
+			$('#messages-list').append('<li class="messages-item new-user"><strong>'+ user.nickname+ '</strong> vient de se déconnecter<span class="messages-date">'+user.h+':'+user.m+'</span></li>');
+		}
+		// scroll to bottom
+		$("#messages-list").animate({ scrollTop: $('#messages-list').prop('scrollHeight') }, 0)
+	})
 
+	socket.on('showCommands', function(commandes){
+		commandes = 'Voici la liste des commandes disponibles :<br>' + commandes
+		printNotif(commandes);
+	})
 
-socket.on('newUser', function(data){
-	$('#messages-list').append(data+ ' vient de se connecter');
-})
-
-// update the number of users and print their names
-socket.on('updateUsersList', function(nbUsers, user){
-	// var $usertpl = $('#user-tpl').html();
-	// $('#user-tpl').remove()
-	// // $('#users-list').append(Mustache.render($usertpl, user));
-	// update number of users
-	$('#users-number').html(nbUsers)
-
-	// add user to the list
-	$('#users-list').append('<li id="'+user.id+'" class="users-item">\
-        <div class="users-avatar">\
-            <img src="'+user.avatar+'" alt="">\
-        </div>\
-        <div class="users-name">\
-            <p>'+user.nickname+'</p>\
-        </div>\
-    </li>')
-
- 	// $('#users-list').html(
- 	// 	Mustache.render($('#users-list').html(), {users: users})
- 	// );
-
-})
-
-socket.on('delUser', function(user){
-	$('#'+user.id).remove();
-})
-
-//send message
-$('#messages-form').submit(function(e){
-	e.preventDefault();
-	$message = $('#message').val();
-	if($message.length > 0){
-		socket.emit('sendMessage', {message : $message})
+	var printNotif = function(notif){
+		$('#messages-list').append('<li class="messages-item"><p>'+notif+'</p></li>');
 	}
-	$('#message').val('');
 
-})
+	// update the number of users and print their names
+	socket.on('updateUsersList', function(nbUsers, user){
 
-// receive message
-socket.on('newMessage', function(message){
-	$('#messages-list').append('<li class="messages-item">\
-        <div class="messages-message">\
-            <div class="messages-avatar">\
-                <img src="'+message.avatar+'">\
-            </div>\
-            <div class="messages-content">\
-                <p><span class="messages-author">'+message.nickname+'</span><span class="messages-date">'+message.h+':'+message.m+'</span></p>\
-                <p>'+message.msg+'</p>\
-            </div>\
-        </div>\
-    </li>')
-    $("#messages-list").animate({ scrollTop: $('#messages-list').prop('scrollHeight') }, 0)
+		// update number of users
+		$('#users-number').html(nbUsers)
+
+		// add user to the list
+		var html = '<li id="'+user.id+'" class="users-item">\
+	        <div class="users-avatar">\
+	            <img src="'+user.avatar+'" alt="">\
+	        </div>\
+	        <div class="users-name">\
+	            <p>'+user.nickname+'</p>\
+	        </div>\
+	    </li>'
+	    $(html).hide().appendTo('#users-list').fadeIn(500)
+
+
+	})
+
+	socket.on('rename', function(user){
+		var html = '<li id="'+user.id+'" class="users-item">\
+	        <div class="users-avatar">\
+	            <img src="'+user.avatar+'" alt="">\
+	        </div>\
+	        <div class="users-name">\
+	            <p>'+user.nickname+'</p>\
+	        </div>\
+	    </li>'
+		$("#" + user.id).empty().append(html)
+	})
+
+	socket.on('newUserInChan', function(data){
+		if(data.present){
+			$('#messages-list').append('<li class="messages-item new-user"><strong>'+ data.nickname+ '</strong> vient d\'entrer dans le salon<span class="messages-date">'+data.h+':'+data.m+'</span></li>')
+		}else{
+			$('#messages-list').append('<li class="messages-item new-user"><strong>'+ data.nickname+ '</strong> a quitté le salon<span class="messages-date">'+data.h+':'+data.m+'</span></li>')
+		}
+	})
+
+	socket.on('renameHeader', function(nickname){
+		$('#username').html(nickname)
+	})
+
+	socket.on('welcome', function(nickname, channel){
+		msg = 'Bienvenue sur my_irc <strong>' +nickname+ '</strong> ! Vous êtes dans le salon <strong>'+channel+'</strong><br>Tapez <strong>/help</strong> pour obtenir la liste des commandes disponibles'
+		printNotif(msg)
+	})
+
+	socket.on('delUser', function(nbUsers, user){
+		$('#users-number').html(nbUsers)
+
+		$('#'+user.id).fadeOut(500,function(){
+			$(this).remove();
+		})
+
+	})
+
+	//send message
+	$('#messages-form').submit(function(e){
+		e.preventDefault();
+		$message = $('#message').val();
+		if($message.length > 0){
+			socket.emit('sendMessage', $message, function(data){
+				// if an error war raised
+				var html = '<li class="messages-item">\
+		            <div class="messages-content messages-error">\
+		                <p>'+data+'</p>\
+		            </div>\
+			    </li>'
+			    $('#messages-list').append(html)
+			})
+		}
+		$('#message').val('');
+		$('#message').focus()
+
+	})
+
+	socket.on('listChannel', function(channels){
+		$('#channels-list').empty()
+		for(var i in channels){
+			var html = '<li class="channels-item">'
+		    if(channels[i].active){
+		    	html += '<span class="active">' +channels[i].name + '</span>'
+		    }
+		    else{
+		    	html += channels[i].name
+		    	html += '<button id="'+channels[i].id+'"class="btn btn-primary btn-join">Rejoindre</button>'
+		    }
+		    html += '</li>'
+			$('#channels-list').append(html)
+		}
+		$('.btn-join').click(function(){
+			var id = $(this).attr('id')
+			socket.emit('joinChannel', id)
+		})
+	});
+
+
+
+	socket.on('joinChannel', function(channelName){
+		joinChannel(channelName)
+	})
+
+	var joinChannel = function(channel){
+		$('#messages-list').empty();
+		printNotif('Vous avez rejoint le channel ' + channel)
+	}
+
+	// receive message
+	socket.on('newMessage', function(message){
+		$('#messages-list').append('<li class="messages-item">\
+	        <div class="messages-message">\
+	            <div class="messages-avatar">\
+	                <img src="'+message.avatar+'">\
+	            </div>\
+	            <div class="messages-content">\
+	                <p><span class="messages-author">'+message.nickname+'</span><span class="messages-date">'+message.h+':'+message.m+'</span></p>\
+	                <p>'+message.msg+'</p>\
+	            </div>\
+	        </div>\
+	    </li>')
+	    $("#messages-list").animate({ scrollTop: $('#messages-list').prop('scrollHeight') }, 0)
+
+
+	})
+	socket.on('sendPm', function(data){ // /msg command
+		$('#messages-list').append('<li class="messages-item">\
+	        <div class="messages-message">\
+	            <div class="messages-avatar">\
+	                <img src="'+data.avatar+'">\
+	            </div>\
+	            <div class="messages-content">\
+	                <p><span class="messages-author">'+username+'</span><span class="messages-date">'+data.h+':'+data.m+'</span></p>\
+	                <p>'+data.msg+'</p>\
+	            </div>\
+	        </div>\
+	    </li>')
+	    $("#messages-list").animate({ scrollTop: $('#messages-list').prop('scrollHeight') }, 0)
+	})
+
+	socket.on('displayListChannels', function(data){ // /list command
+		var string = '';
+		if(data.length > 0){
+			for(var i = 0; i < data.length; i++){
+				if(i != data.length){
+					string += data[i] + '<br>'
+				}
+				else{
+					string += data[i]
+				}
+			}
+		}
+		else{
+			string = 'Aucun résultat'
+		}
+		printNotif(string)
+	})
+
+	socket.on('listUsers', function(data){
+		var string = 'Liste des utilisateurs dans le salon :<br>';
+		for(var i = 0; i < data.length; i++){
+			if(i != data.length){
+				string += data[i] + '<br>'
+			}
+			else{
+				string += data[i]
+			}
+		}
+		printNotif(string)
+	})
+
 })
 
 
--- server.js
+++ server.js
@@ -19,15 +19,36 @@ app.get('/', function(request, response){
 
 var sockets = {}
 var users = {}
+
+
 var nbUsers = 0
 
+var commandes = '\
+<strong>/nick _nickname_ :</strong> définit le surnom de l’utilisateur au sein du channel<br>\
+<strong>/list [string] :</strong> liste les channels disponibles sur le serveur. N’affiche que les channels contenant la chaîne "string" si celle-ci est spécifiée.<br>\
+<strong>/join _channel_ :</strong> rejoint un channel sur le serveur<br>\
+<strong>/part _channel_ :</strong> quitte le channel<br>\
+<strong>/users :</strong> liste les utilisateurs connectés au channel<br>\
+<strong>/msg _nickname_ _message_ :</strong> envoie un message à un utilisateur spécifique'
 io.on('connection', function(socket){
 
+	var present = false
 	// user already exists
 	for(var i in users){
 		socket.emit('updateUsersList', nbUsers, users[i])
 	}
 	var me = {}
+	var channels = {
+		"Général":{name: 'Général', id:"general", active: true},
+		'Jeux': {name: 'Jeux', id:"jeux", active: false},
+		'Sport': {name: 'Sport', id:"sport", active: false},
+		'Cinéma':{name: 'Cinéma', id:"cinema", active: false},
+		'Musique':{name: 'Musique', id:"musique", active: false},
+		'Cuisine':{name: 'Cuisine', id:"cuisine", active: false},
+		'Poneys':{name: 'Poneys', id:"poneys", active: false}
+	}
+
+	socket.join('Général')
 	socket.on('login', function(user, callback){
 		if(user.nickname in sockets){
 			callback("Votre pseudo est déjà pris !")
@@ -42,67 +63,214 @@ io.on('connection', function(socket){
 			// }
 			else{
 				// user is valid and can now log in
+				var date = new Date();
 				callback(false)
 				user.avatar = gravatar.url(user.email, {s:60})
 				user.id = socket.id
 				me = user
+				present = true
+				// notifiy already logged users that a new user logged in
+				for(var i in sockets){
+					sockets[i].emit('newUser', {nickname: me.nickname, present:present, h: date.getHours(),	m: (date.getMinutes()<10?'0':'') + date.getMinutes()})
+				}
 				socket.nickname = user.nickname
 				sockets[user.nickname] = socket
-				users[user.email] = user
-				console.log(user)
-				console.log(me)
+				me.currentChannel = 'Général'
+				users[user.email] = me
+				console.log(users)
 				++nbUsers
 				io.emit('updateUsersList', nbUsers, me);
-				// for(var i in sockets){
-				// 	if(i != socket.nickname){
-				// 		sockets[i].emit('newUser', socket.nickname);
-				// 	}
-				// 	// sockets[i].emit('updateUsersList', nbUsers, users);
-				// }
+				socket.emit('welcome', me.nickname, me.currentChannel)
+				socket.emit('listChannel', channels)
+
+
 			}
 		}
 	})
 
-	// for(var i in users){
-	// 	socket.emit('updateUsersList', nbUsers, users[i])
-	// }
 
-	socket.on('sendMessage', function(message){
-		date = new Date();
-		io.emit('newMessage', {
-			msg : message.message,
-			nickname : socket.nickname,
-			avatar : socket.avatar,
-			h: date.getHours(),
-			m: date.getMinutes()
-		})
+	socket.on('sendMessage', function(data, callback){
+		var date = new Date();
+		var msg = data.trim()
+		var nicknames = []
+		for(var i in users){
+			nicknames.push(users[i].nickname)
+		}
+		// PM
+		if(msg.substring(0,5) === "/msg "){
+			msg = msg.substring(5)
+			var index = msg.indexOf(' ')
+			if(index !== -1){
+				console.log('presque')
+				var receiver = msg.substring(0, index)
+				msg = msg.substring(index + 1)
+				console.log(receiver)
+				if(nicknames.indexOf(receiver) !== -1){
+					console.log('new pm')
+					var pm = {
+						receiver: receiver,
+						sender:me.nickname,
+						avatar : me.avatar,
+						msg:msg,
+						h: date.getHours(),
+						m: (date.getMinutes()<10?'0':'') + date.getMinutes()
+					}
+					sockets[receiver].emit('sendPm', pm)
+					socket.emit('sendPm', pm)
+				}
+				else{
+					callback("Cet utilisteur n'existe pas !")
+				}
+			} else {
+				callback("Veuillez enter un message !")
+			}
+		}
+		else if(msg === "/users"){
+			// print users connected to current channel
+
+			var nicknames = []
+			for(var i in users){
+				if(users[i].currentChannel == me.currentChannel){
+					nicknames.push(users[i].nickname)
+				}
+			}
+			io.emit('listUsers', nicknames)
+
+		}
+		else if (msg.substring(0,6) === "/nick "){
+			// rename users
+			var newNick = msg.substring(6)
+			var valid;
+			for(var i in users){
+				if(users[i].nickname == newNick){
+					callback('Pseudo déjà pris !')
+					valid=false
+				}
+			}
+			me.nickname = newNick
+			users[me.email] = me
+			io.emit('rename', me);
+			socket.emit('renameHeader', me.nickname)
+		}
+		else if(msg.substring(0,5) === "/list"){
+
+			msg = msg.substring(5).trim()
+			var response
+
+			if(msg.length > 0){ // if a string paramter is present
+				response = listChannels(msg)
+			}
+			else{
+				response = listChannels(false)
+			}
+			socket.emit('displayListChannels', response)
+
+		}
+		else if(msg.substring(0,6) === "/join "){ // join channel
+			channelName = msg.substring(6)
+
+			if(channelName in channels){ // that channel exists
+				joinChannel(channelName)
+			}
+			else{
+				callback("Ce salon n'existe pas")
+			}
+		}
+		else if(msg.substring(0,6) === "/part "){ // leave channel
+			channelName = msg.substring(6)
+			if(channelName in channels){ // that channel exists
+				socket.leave(channelName)
+				channels[channelName].active = false
+				me.currentChannel = 'Général'
+				users[me.email] = me
+				channels[me.currentChannel].active = true
+				socket.emit('leaveChannel', channelName)
+				// list channels
+				socket.emit('listChannel', channels)
+			}
+			else{
+				callback("Ce salon n'existe pas")
+			}
+		}
+		else if(msg === "/help"){
+			socket.emit('showCommands', commandes)
+		}
+		else if (msg.substring(0,1) === "/"){
+			// unknown command
+			callback('Commande inconnue')
+		}
+		else{
+
+			io.in(me.currentChannel).emit('newMessage', {
+				msg : msg,
+				nickname : me.nickname,
+				avatar : me.avatar,
+				h: date.getHours(),
+				m: (date.getMinutes()<10?'0':'') + date.getMinutes()
+			})
+		}
 	})
 
 	// refresh or exit browser
 	socket.on('disconnect', function(data){
 		if(!socket.nickname) return;
 
-
+		var date = new Date();
 		delete users[me.email]
 		delete sockets[socket.nickname]
-		io.emit('delUser', me)
 		--nbUsers
-		// if(typeof users != "undefined" && users.length > 0){
-		// 	for(var i=0; i< users.length; i++){
-		// 		if(users[i].nickname == socket.nickname){
-		// 			// empty users array when disconnect
-		// 			users.splice(i, 1)
-		// 		}
-		// 	}
-		// }
-
-		// socket.broadcast.emit('updateUsersList', nbUsers, users);
+		present = false;
+		for(var i in users){
+			sockets[i].emit('newUser', {nickname: me.nickname, present:present, h: date.getHours(),	m: (date.getMinutes()<10?'0':'') + date.getMinutes()})
+		}
+		io.emit('delUser',nbUsers, me)
 	})
+
+	socket.on('joinChannel', function(channelId){ // join channel by pressing button
+		for(var i in channels){
+			if(channels[i].id == channelId){
+				console.log(i)
+				joinChannel(i)
+			}
+		}
+	})
+	var joinChannel = function(channelName){
+		date = new Date()
+		socket.leave(me.currentChannel)
+		socket.join(channelName)
+		channels[channelName].active = true
+
+		socket.broadcast.in(me.currentChannel).emit('newUserInChan', {nickname: me.nickname, present:false, h: date.getHours(),	m: (date.getMinutes()<10?'0':'') + date.getMinutes()})
+		channels[me.currentChannel].active = false
+		me.currentChannel = channelName
+		users[me.email] = me
+		// list channels
+		socket.emit('listChannel', channels)
+		socket.emit('joinChannel', channelName)
+		if(channelName != 'Général'){
+			socket.broadcast.in(channelName).emit('newUserInChan', {nickname: me.nickname, present:true, h: date.getHours(),	m: (date.getMinutes()<10?'0':'') + date.getMinutes()})
+		}
+	}
+
+	var listChannels = function(search){
+		results = []
+		for(var i in channels){
+			if(!search){ // one parameter after /list = search the string in channels list
+				results.push(i)
+			}
+			else{
+				if(i.indexOf(search) !== -1){
+					results.push(i)
+				}
+			}
+		}
+		return results
+	}
+
 
 })
 
-
-function validateEmail(email) {
+var validateEmail = function(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
